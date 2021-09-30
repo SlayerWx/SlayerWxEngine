445cbc5bf85908eb34594321b238391a5559f30a
SlayerWx <maxi_cs_95@hotmail.com>
2021-09-29 23:35:03 -0300

    create window

---
+++ SlayerWxEngine/SlayerWxEngine/Src/window/window.h
@@ -0,0 +1,17 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+#include "export/export.h"
+struct GLFWmonitor;
+struct GLFWwindow;
+class SlayerWxEngine_API Window
+{
+private:
+	GLFWwindow* window;
+public:
+	Window();
+	Window(int width, int height, const char* title, GLFWmonitor* monitor, GLFWwindow* share);
+	void WindowContext();
+	bool ShouldClose();
+	GLFWwindow* GetWindow();
+};
+#endif
