6a40ac5ceaeb475626a4118603e5593d7f03259a
SlayerWx <maxi_cs_95@hotmail.com>
2021-09-30 03:21:06 -0300

    CompileShader and CreateProgram

--- SlayerWxEngine/Src/renderer/renderer.cpp
+++ SlayerWxEngine/Src/renderer/renderer.cpp
@@ -1,7 +1,9 @@
 #include "renderer.h"
 #include <glew.h>
 #include <glfw3.h>
-
+#include <iostream>
+#include <fstream>
+#include <sstream>
 Renderer::Renderer()
 {
 }
@@ -24,4 +26,57 @@ void Renderer::BindBuffers()
 	glBindVertexArray(VAO);// Specifies the name of the vertex array to bind
 	glBindBuffer(GL_ARRAY_BUFFER, VBO); //first: type buffer to bound
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); //second: the buffer
+}
+unsigned int CompileShader(unsigned int type, const char* shaderPath) { //first: ShaderType(Fragment, vertex)
+																		//second:Dir to archive
+	unsigned int id = glCreateShader(type); // Create Shader
+	
+	std::string sourceShaderCode; //store source archive
+	
+	std::ifstream sourceShaderFile; // interact to archive
+	
+	sourceShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	
+	try {
+	    sourceShaderFile.open(shaderPath); 
+	    std::stringstream shaderStream;
+	
+		shaderStream << sourceShaderFile.rdbuf(); //conversion to StreamString
+	
+	    sourceShaderFile.close();
+	
+	    sourceShaderCode = shaderStream.str(); //conversion to string
+	}
+	catch (std::ifstream::failure& e) {
+	    std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+	}
+	
+	const char* srcCode = sourceShaderCode.c_str(); // conversion to char
+	
+	glShaderSource(id, 1, &srcCode, nullptr); //Set source to Shader
+											// First: Shader, Second: conunt elements in the string
+											// three: Specifies an array of pointers to strings containing the source
+											// four: Specifies an array of string lengths
+	glCompileShader(id); //Complie Shader
+	
+	return id;
+}
+
+void Renderer::CreateProgram(const char* vertexShaderPath, const char* pixelShaderPath) {
+																					//first: vertex archive
+																					//second: fragmentShader
+	program = glCreateProgram(); // create program
+	unsigned int vertex = CompileShader(GL_VERTEX_SHADER, vertexShaderPath);
+	unsigned int fragment = CompileShader(GL_FRAGMENT_SHADER, pixelShaderPath);
+
+	glAttachShader(program, vertex); //attach with program
+	glAttachShader(program, fragment); //attach with program
+	glLinkProgram(program); // Link with OpenGL
+	glValidateProgram(program); //validate
+	glUseProgram(program); 
+
+	glDetachShader(program, vertex); //UnAttach
+	glDetachShader(program, fragment);//UnAttach
+	glDeleteShader(vertex); //delete
+	glDeleteShader(fragment); //delete
 }
--- SlayerWxEngine/Src/renderer/renderer.h
+++ SlayerWxEngine/Src/renderer/renderer.h
@@ -7,12 +7,14 @@ private:
 public:
 	Renderer();
 	void BeginDraw();
-	unsigned int VBO; // VertexBufferObject
-	unsigned int VAO; // VertexArrayObject
-	unsigned int EBO; // ElementsBufferObject
-	unsigned int program; // ProgramShader
+	unsigned int VBO = 0; // VertexBufferObject
+	unsigned int VAO = 0; // VertexArrayObject
+	unsigned int EBO = 0; // ElementsBufferObject
+	unsigned int program = 0; // ProgramShader
 	void CreateBuffers();
 	void BindBuffers();
+	unsigned int CompileShader(unsigned int type, const char* shaderPath);
+	void CreateProgram(const char* vertexShaderPath, const char* pixelShaderPath);
 };
 // settings
 //const unsigned int SCR_WIDTH = 800;
