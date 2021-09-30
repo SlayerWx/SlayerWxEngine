bb5dcc0e0b19b61bc5687527aea78e24066afabe
SlayerWx <maxi_cs_95@hotmail.com>
2021-09-30 04:59:10 -0300

    create Entity and Shape

---
+++ SlayerWxEngine/Src/entity/Entity.h
@@ -0,0 +1,13 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+#include "export/export.h"
+class SlayerWxEngine_API Entity
+{
+public:
+	Entity();
+	~Entity();
+
+private:
+
+};
+#endif
---
+++ SlayerWxEngine/Src/shape/Shape.h
@@ -0,0 +1,13 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+#include "export/export.h"
+class SlayerWxEngine_API Shape
+{
+public:
+	Shape();
+	~Shape();
+
+private:
+
+};
+#endif
